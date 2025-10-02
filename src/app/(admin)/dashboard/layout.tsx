c6aea980057846c48a0ded1c6c150710189ed8bb
Md Repon <repon7253@gmail.com>
2025-10-02 19:47:15 +0600

    add admin sidebar

---
+++ src/app/(admin)/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import AdminSidebar from '@/components/Admin/AdminSidebar';
+import React from 'react';
+
+const AdminDashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='bg-gray-100 min-h-screen flex gap-4'>
+      <AdminSidebar />
+      <div className='mt-6'>{children}</div>
+    </div>
+  );
+};
+
+export default AdminDashboardLayout;
